af633856505e4d0f8384bb365e324768c5a7f142
Maythee Anegboonlap <natkung@gmail.com>
2013-06-01 03:29:45 -0400

    Updated express.js to version 3.

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
+// All dependencies live here
+var express = require('express'),
+    path = require('path'),
+    less_middleware = require('less-middleware'),
+    routes = require('./routes');
 
-/**
- * Module dependencies.
- */
+var flash = require('connect-flash');
 
-var express = require('express')
-  , less_middleware = require('less-middleware')
-  , routes = require('./routes');
+var app = express();
 
-var flash = require('connect-flash')
-  , MongoStore = require('connect-mongo')(express);
+// passport and security session
+var cookieSecret = 'tUjurat6';
 
-var passport = require('passport')
-  , LocalStategy = require('passport-local').Strategy;
-
-var app = module.exports = express.createServer();
-
-// Configuration
+// express.js configuration
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler());
   app.use(less_middleware({
     src: __dirname + '/public',
     force: true
   }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function() {
+  app.use(less_middleware({src: __dirname + '/public'}));
 });
+
 
 app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser(cookieSecret));
+  app.use(flash());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
-
-// Routes
 
 app.get('/', routes.index);
 
-app.listen(process.env.PORT || 3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// server codes
+app.listen(app.get('port'), function () {
+  console.log ('Express server listening on port ' + app.get('port'));
+});
