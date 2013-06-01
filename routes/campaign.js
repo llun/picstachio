aa4a513a4ac626e6063f1b8c492172c50c6672ff
Maythee Anegboonlap <natkung@gmail.com>
2013-06-01 09:49:51 -0400

    Finished upload pictures.

--- routes/campaign.js
+++ routes/campaign.js
@@ -1,3 +1,13 @@
+var knox = require('knox'),
+    q = require('q'),
+    _ = require('underscore');
+
+var client = knox.createClient({
+    key: 'AKIAJMWRPBMTW5L7HBJA',
+    secret: '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',
+    bucket: 'picstachio'
+  });
+
 var Campaign = require('../models/campaign');
 
 module.exports = {
@@ -10,6 +20,7 @@ module.exports = {
   // API zone
   add: function (req, res) {
 
+    var campaign = null;
     var input = {
       name: req.body.name,
       description: req.body.description,
@@ -23,17 +34,32 @@ module.exports = {
       }
     }
 
-    var campaign = new Campaign(input);
-    campaign.save(function (err) {
-      if (err) {
-        res.json(err);
-      }
-      else {
+    var maps = _.map(req.files.files, function (file) {
+      return q.nfcall(client.putFile.bind(client),
+              file.path,
+              '/res/' +  (new Date().getTime()) + '/' + file.name,
+              { 'x-amz-acl': 'public-read' });
+    });
+    q.all(maps)
+      .then(function (out) {
+        input.images = [];
+        _.each(out, function (v) {
+          input.images.push(v.req.url);
+        });
+        campaign = new Campaign(input);
+        return q.nfcall(campaign.save.bind(campaign));
+      })
+      .then(function () {
+        console.log ('Done');
         res.status(302);
         res.header('Location', '/');
         res.json(campaign);
-      }
-    });
+      })
+      .fail(function (err) {
+        console.error (err);
+        res.json(err);
+      })
+      .done();
 
   }
 
